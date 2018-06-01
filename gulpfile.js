e5235ace2060b72baa92fade0bb8aed659605537
Thiago Menezes <thiago.cria.oliveira@gmail.com>
2018-06-01 02:30:43 -0400

    criando a primeira página do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch("./**/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./**/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function () {
     return gulp.src('./distribuicao/estilo.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./ativos/css'));
+    .pipe(gulp.dest('./app/css'));
 });
 
 gulp.task('default', ['serve', 'sass']);
