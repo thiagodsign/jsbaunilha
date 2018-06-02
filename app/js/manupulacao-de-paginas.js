c941bd6e32a6d7796a4add058798e1d1dfe73b6e
thiagodsign <thiago.cria.oliveira@gmail.com>
2018-06-02 02:27:58 -0400

    ajuste no relogio

--- app/js/manupulacao-de-paginas.js
+++ app/js/manupulacao-de-paginas.js
@@ -6,6 +6,7 @@ Router(rotas).init(['/bateria-virtual.html']);
 
 function incluirConteudo(nomeDaPagina) {
   $.get('/jsbaunilha/paginas/' + nomeDaPagina)
+  // $.get('/paginas/' + nomeDaPagina)
       .then(function (conteudo) {
           $('#doc-conteudo-principal').empty().append(conteudo);
       });
